9a3fc4cc280a4634c9543b2fcd89cb551a750f90
ffrostflame <80861876+ffrostflame@users.noreply.github.com>
2022-08-15 14:11:29 -0400

    Built and published by Moonwave

--- assets/js/a5f01389.3103ec9c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[403],{97868:function(e){e.exports=JSON.parse('{"sourceUrl":"https://github.com/ffrostflame/bridgenet//blob/main","baseUrl":"/BridgeNet/","classOrder":[],"apiCategories":[]}')}}]);
---
+++ assets/js/ee4438a9.d67fb303.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[469],{31277:e=>{e.exports=JSON.parse('[{"value":"Types","id":"types","level":2},{"value":"Properties","id":"properties","level":2},{"value":"Functions","id":"functions","level":2},{"value":".SetReceiveRate","id":"SetReceiveRate","level":3},{"value":".SetSendRate","id":"SetSendRate","level":3}]')}}]);
