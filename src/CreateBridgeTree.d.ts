12ff66159799c73bcc19f69a36c1552b2076d5df
Weldify <ian.kharlak@gmail.com>
2022-10-27 21:23:10 +0300

    redo roblox-ts typings

--- src/CreateBridgeTree.d.ts
+++
@@ -1,7 +0,0 @@
-import bridge from 'Bridge';
-
-type Dictionary<V> = {[string: string]: V}
-type TreeLeaf = Dictionary<bridge>
-type Tree = Dictionary<bridge | Dictionary<TreeLeaf>>
-
-type CreateBridgeTree = (tree: Tree) => keyof typeof tree
